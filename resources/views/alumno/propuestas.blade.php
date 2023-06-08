4ee8004721ea3fabef1f384c5b8dd665cfd66bb9
carlos-guerra-a <135287095+carlos-guerra-a@users.noreply.github.com>
2023-06-08 00:27:44 -0400

    Estados
    Actualización de estados
    
    Co-Authored-By: JoaquinVergaraE <135592392+JoaquinVergaraE@users.noreply.github.com>
    Co-Authored-By: ian-usm <135771194+ian-usm@users.noreply.github.com>

--- resources/views/alumno/propuestas.blade.php
+++ resources/views/alumno/propuestas.blade.php
@@ -6,9 +6,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>USM - Propuestas</title>
 </head>
 <body>
+    @php
+        $estados = [1 => 'Esperando Revisión',2=>'Modificar Propuesta',3=>'Rechazado',4=>'Aceptado'];
+    @endphp
     <nav class="navbar bg-primary" data-bs-theme="dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
@@ -31,7 +34,7 @@
                 <div class="card-header" id="heading-{{ $propuesta->id }}">
                     <h2 class="mb-0">
                         <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse-{{ $propuesta->id }}" aria-expanded="true" aria-controls="collapse-{{ $propuesta->id }}">
-                            ID de propuesta: {{ $propuesta->id }} | Estado: {{ $propuesta->estado }}
+                            ID de propuesta: {{ $propuesta->id }} | Estado: {{ $estados[$propuesta->estado] }}
                         </button>
                     </h2>
                 </div>
