ab0b5b5e0c8e125da249c3477023d4bbd888155c
ian-usm <135771194+ian-usm@users.noreply.github.com>
2023-06-09 21:26:41 -0400

    cosas

--- resources/views/profesor/propuestas.blade.php
+++ resources/views/profesor/propuestas.blade.php
@@ -1,33 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+@extends('templates.master')
 
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>USM - Propuestas</title>
-</head>
-<body>
+@section('contenido-principal')
     @php
         $estados = [1 => 'Esperando Revisión',2=>'Modificar Propuesta',3=>'Rechazado',4=>'Aceptado'];
     @endphp
 
-    <nav class="navbar bg-primary" data-bs-theme="dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="https://aula.usm.cl/portada/images/logo-usm_blanco.ba50c1e92c05ce59220ab09bd88a6d5b.svg" alt="Logo" class="d-inline-block align-text-top container-fluid">     
-            </a>
-            <div class="d-flex justify-content-center mb-4">
-                <a href="{{ route('alumno.home') }}" class="btn btn-primary border">Volver</a>
-            </div>
-        </div>
-    </nav>
+    
 
     <div class="text-center mb-4">
         <h1>Propuestas de {{ $alumnos->nombre }} {{ $alumnos->apellido }}</h1>
@@ -60,7 +39,4 @@
                 </div>
             @endforeach
     </div>
-
-
-</body>
-</html>
+@endsection
